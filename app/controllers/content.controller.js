d75d3c8193b8aacff7227eae51775fa376694421
shashikanth077 <shashikanth033@gmail.com>
2024-01-24 00:00:59 +0530

    static content upload integration with api changes

--- app/controllers/content.controller.js
+++ app/controllers/content.controller.js
@@ -26,6 +26,7 @@ exports.getStaticContentJson = async (req, res) => {
       });
     }
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: "Internal server error (Content)" });
   }
 };
