43b003a55183efee68fe012cc38b974288911560
Shashikanth HR <57744709+shashikanth077@users.noreply.github.com>
2024-03-26 22:55:08 +0530

    swagger def for content|order routes

--- app/routes/admin/contentRoutes.js
+++ app/routes/admin/contentRoutes.js
@@ -9,8 +9,54 @@ module.exports = function (app) {
     next();
   });
 
+  /**
+   * @swagger
+   * /api/content:
+   *   get:
+   *     tags: [Content Controller]
+   *     summary: Get static content JSON
+   *     security:
+   *       - basicAuth: []
+   *     responses:
+   *       200:
+   *         description: OK
+   *       401:
+   *         description: Unauthorized
+   *       404:
+   *         description: Not Found
+   *       500:
+   *         description: Server Error
+   */
   app.get("/api/content", controller.getStaticContentJson);
 
+  /**
+   * @swagger
+   * /api/staticupload:
+   *   post:
+   *     tags: [Content Controller]
+   *     summary: Upload static file
+   *     security:
+   *       - basicAuth: []
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         multipart/form-data:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               staticFile:
+   *                 type: string
+   *                 format: binary
+   *     responses:
+   *       200:
+   *         description: OK
+   *       401:
+   *         description: Unauthorized
+   *       404:
+   *         description: Not Found
+   *       500:
+   *         description: Server Error
+   */
   app.post(
     "/api/staticupload",
     [authJwt.verifyToken, authJwt.isAdmin],
